ea45db5874b68b71d0668728a1956e93b695bce8
CrypticCryptid <aege1.1.2002@gmail.com>
2022-11-28 20:33:37 -0800

    Can inspect profiles better
    Added switch-case that based on whichever button is pressed will reflect the associated profile

--- 2DClueClone/Assets/SuspectMenu.cs
+++ 2DClueClone/Assets/SuspectMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SuspectMenu : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class SuspectMenu : MonoBehaviour
     public GameObject journalUI;
     public GameObject suspectsPage;
     public GameObject inspectPage;
+
+    public Sprite[] portaits;
+    public Image pfp; //profile pic. The one in the Inspect Page
 
     // Update is called once per frame
     void Update()
@@ -29,12 +34,43 @@ public class SuspectMenu : MonoBehaviour
 
     public void OpenJournal() {
         journalUI.SetActive(true);
+        suspectsPage.SetActive(true);
+        inspectPage.SetActive(false);
         isOpen = true;
     }
 
     public void Inspect() {
         suspectsPage.SetActive(false);
         inspectPage.SetActive(true);
+
+        string suspect = EventSystem.current.currentSelectedGameObject.name;
+
+        switch(suspect) {
+            case "Suspect1":
+                Debug.Log("Inspecting: " + suspect);
+                pfp.GetComponent<Image>().color = Color.black;
+                break;
+            case "Suspect2":
+                Debug.Log("Inspecting " + suspect);
+                pfp.GetComponent<Image>().color = Color.blue;
+                break;
+            case "Suspect3":
+                Debug.Log("Inspecting " + suspect);
+                pfp.GetComponent<Image>().color = Color.cyan;
+                break;
+            case "Suspect4":
+                Debug.Log("Inspecting " + suspect);
+                pfp.GetComponent<Image>().color = Color.gray;
+                break;
+            case "Suspect5":
+                Debug.Log("Inspecting " + suspect);
+                pfp.GetComponent<Image>().color = Color.magenta;
+                break;
+            case "Suspect6":
+                Debug.Log("Inspecting " + suspect);
+                pfp.GetComponent<Image>().color = Color.red;
+                break;
+        }
 
         //Set UI's sprite to Item's sprite
     }
