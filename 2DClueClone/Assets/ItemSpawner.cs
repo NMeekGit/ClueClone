a326fa08fcf876e050403342e6c3d238207c1afa
CrypticCryptid <aege1.1.2002@gmail.com>
2022-12-01 17:16:16 -0800

    Items spawn in randomly
    Items now spawn in randomly. They are lacking the ability to be bloody for the time being but should be resolved shortly

--- 2DClueClone/Assets/ItemSpawner.cs
+++ 2DClueClone/Assets/ItemSpawner.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class ItemSpawner : MonoBehaviour
 {
     public GameObject[] items;
+    public Sprite[] bloodySprites;
     public Transform[] spawnpoints;
+
+    public MurderSelect murderManager;
     
     private List<int> usedNums = new List<int>();
     private int ranNum;
-    
+    private int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        /*for(int i = 0; i < items[].Length; i++) {
+        while(usedNums.Count < items.Length) {
             UniqueNums();
         }
-        
+
         foreach(int i in usedNums) {
-            Instantiate(items[i], spawnpoints[i]);
-        }
-        for(int i = 0; i < spawnpoints.Length; i++) {
-            Instantiate(items[usedNums[i]], spawnpoints[i]);
+            int temp = i;
+            Instantiate(items[temp], spawnpoints[count].position, Quaternion.identity);
+            count++;
         }
 
-        */
+        //Debug.Log("MurderSelect.GetCharacterItems(1): " + murderManager.GetMurderItems(1));
+        //items[murderManager.GetCharacterItems(1)].sprite = bloodySprites[1];
+        
     }
 
     void UniqueNums() {
